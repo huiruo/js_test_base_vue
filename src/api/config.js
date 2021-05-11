a9e9b751383daa2abdd85b34967803e3a8c87188
huiruo <2196411859@qq.com>
2021-05-11 11:25:20 +0800

    fix:修改ui

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 //外测
-// let API_BASE = "https://h5-test.jiaoyoushow.com"
+let API_BASE = "https://h5-test.jiaoyoushow.com"
 //预发布
 // let API_BASE = "https://h5-preview.jiaoyoushow.com"
 //正式
-let API_BASE = "https://h5-ddos.1kyou.cn"
+// let API_BASE = "https://h5-ddos.1kyou.cn"
 
 export default API_BASE;
